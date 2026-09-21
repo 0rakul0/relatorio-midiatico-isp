02c8aa9ace7f70d08552f49f54472f881ccf93ad
ANJOS, J. S. <0rakul0render@gmail.com>
2026-09-21 11:55:20 -0300

    Exporta chat global do acervo

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -11,7 +11,7 @@ from .collection.common import canonicalize, query_window
 from .collection.web import collect_web
 from .collection.youtube import collect_media_sources
 from .collection.youtube_helpers import is_youtube_url, youtube_tasks_for_execution
-from .corpus_chat import chat_with_corpus, list_chat_projects
+from .corpus_chat import chat_with_all_corpus, chat_with_corpus, list_chat_projects
 from .execution_profile import effective_execution_profile, execution_flags
 from .metrics import metrics
 from .pipeline import run_full_methodology
@@ -24,6 +24,7 @@ __all__ = [
     "cached_report_for_project",
     "cached_report_for_topic",
     "canonicalize",
+    "chat_with_all_corpus",
     "chat_with_corpus",
     "classify_with_llm",
     "collect_media_sources",
